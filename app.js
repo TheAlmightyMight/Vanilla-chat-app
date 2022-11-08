60f2005a0bf37e4615b7b3bf533ad3a34995ae9e
Klimenty Karavaev <klimentykk@gmail.com>
2022-11-08 19:47:49 +0400

    Commit previous changes

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
 const app = express();
-const port = 8080;
+const path = require("path");
+const port = 3000;
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+const arr = [];
+
+app.use("/", (req, res, next) => {
+  console.log("yay");
+  next();
 });
+
+const isAuthenticated = (req, res, next) => {
+  console.log(arr);
+  const cookie = arr.find((el) => el.name === req.headers.cookie.name);
+
+  if (cookie) {
+    next();
+  } else {
+    res.send("Not authroized").status(401);
+  }
+};
+
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/auth", (req, res) => {
+  console.log(req.headers.cookie);
+  res.cookie("rememberme", "1", {
+    expires: new Date(Date.now() + 900000),
+    httpOnly: true,
+    sameSite: true,
+  });
+  arr.push({ rememberme: "1" });
+  res.end();
+});
+
+app.get("/private", isAuthenticated, (req, res) => {
+  console.log(req.headers.cookie);
+  res.send("Private route");
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
