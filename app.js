0cccae47e2e0105730b2055e854a4fe8b7c0b04c
Klimenty Karavaev <klimentykk@gmail.com>
2022-11-19 12:58:56 +0400

    Commit changes

--- app.js
+++ app.js
@@ -2,43 +2,40 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3000;
+const event = require("node:events");
 
-const arr = [];
+class Logger extends event.EventEmitter {
+  constructor() {
+    super();
+    this.messages = [];
+  }
+
+  log(...messages) {
+    this.emit("log", messages);
+  }
+}
+
+const logger = new Logger();
+
+logger.on("log", (args) => {
+  args.forEach((el) => {
+    console.log(
+      el ?? "Request object was not populated with the provided property"
+    );
+  });
+});
 
 app.use("/", (req, res, next) => {
-  console.log("yay");
+  logger.log(req.body, req.params);
   next();
 });
 
-const isAuthenticated = (req, res, next) => {
-  console.log(arr);
-  const cookie = arr.find((el) => el.name === req.headers.cookie.name);
-
-  if (cookie) {
-    next();
-  } else {
-    res.send("Not authroized").status(401);
-  }
-};
-
-app.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
 
-app.get("/auth", (req, res) => {
-  console.log(req.headers.cookie);
-  res.cookie("rememberme", "1", {
-    expires: new Date(Date.now() + 900000),
-    httpOnly: true,
-    sameSite: true,
-  });
-  arr.push({ rememberme: "1" });
-  res.end();
-});
-
-app.get("/private", isAuthenticated, (req, res) => {
-  console.log(req.headers.cookie);
-  res.send("Private route");
+app.get("/page/:id", (req, res) => {
+  res.send(`Hello page number ${req.params.id}`);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
